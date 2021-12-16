a39bf4a6f9987725e79a2510e437413488473b4e
matias oliva <matyasoliva06@gmail.com>
2021-12-16 15:43:29 -0300

    Practica manipulacion de archivos

---
+++ src/timer/Archivos.java
@@ -0,0 +1,69 @@
+
+
+/*
+* Codigo practica manipulacion de archivos. Crear y escribir archivo
+* */
+
+package timer;
+
+import javax.swing.*;
+import java.io.*;
+
+public class Archivos {
+
+    public static void main(String[] args) {
+
+
+        File archivo;
+        FileWriter escribir;
+        PrintWriter linea;
+        String nombre, apellido, email;
+
+
+
+        archivo = new File("archivo.txt");
+
+        if (!archivo.exists()) {
+            try {
+
+                archivo.createNewFile();
+                escribir = new FileWriter(archivo);
+                linea = new PrintWriter(escribir);
+                nombre = JOptionPane.showInputDialog("Digite su nombre","Ingrese nombre aqui");
+                apellido = JOptionPane.showInputDialog("Digite su apellido","Ingrese apellido aqui");
+                email = JOptionPane.showInputDialog("Digite su email","ingrese email aqui");
+                linea.println("Nombre: " + nombre);
+                linea.println("Apellido: " + apellido);
+                linea.println("Email: " + email);
+                linea.println("");
+                linea.close();
+                escribir.close();
+
+            } catch (IOException e) {
+                System.err.println("No se a podido crear el archivo");
+
+            }
+
+        } else {
+            try {
+                escribir = new FileWriter(archivo,true); //true para escribir archivo abajo de las lineas ya escritas
+                linea = new PrintWriter(escribir);
+                nombre = JOptionPane.showInputDialog("Digite su nombre","Ingrese nombre aqui");
+                apellido = JOptionPane.showInputDialog("Digite su apellido","Ingrese apellido aqui");
+                email = JOptionPane.showInputDialog("Digite su email","Ingrese email aqui");
+                linea.println("Nombre: " + nombre);
+                linea.println("Apellido: " + apellido);
+                linea.println("Email: " + email);
+                linea.println("");
+                linea.close();
+                escribir.close();
+            } catch (IOException e) {
+                System.err.println("No se a podido crear el archivo");
+            }
+
+
+
+
+        }
+    }
+}
